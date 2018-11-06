844651747a240e18b92322e374e7627e95472ba5
Chris <chriseckert.ce@gmail.com>
2018-11-06 22:40:57 +0100

    added routing packages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+
 import Login from './pages/Login';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Login />
       </div>
     );
   }
