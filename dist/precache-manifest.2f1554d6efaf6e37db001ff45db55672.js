75906ba5a4c2d6cd681773d37f8f39821b9381b6
Chris Eckert <ce@morningtrain.dk>
2018-11-21 13:58:40 +0100

    deployment to firebase

---
+++ dist/precache-manifest.2f1554d6efaf6e37db001ff45db55672.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bc264189b8724f6d89a9",
+    "url": "/static/js/main.bc264189.chunk.js"
+  },
+  {
+    "revision": "3534eaa7fad80d8a341b",
+    "url": "/static/js/1.3534eaa7.chunk.js"
+  },
+  {
+    "revision": "bc264189b8724f6d89a9",
+    "url": "/static/css/main.0ac88676.chunk.css"
+  },
+  {
+    "revision": "8534cf3f25775a8b53ab1d5a3255d136",
+    "url": "/index.html"
+  }
+];
